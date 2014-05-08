b583c21284dabe2e25f03e12651a95eb03add1a4
Rajesh <72iarud@gmail.com>
2014-05-09 02:11:22 +0800

    Fix 1 for Heroku:precompile assets failed

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,14 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+# config/application.rb
+config.assets.initialize_on_precompile = false
 
 module Crowdmunch
   class Application < Rails::Application
